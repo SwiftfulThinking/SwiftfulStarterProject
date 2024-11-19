f8bf91d220ce3ab45d36daec44fa1e0a2b6c7691
Nick <hello@swiftful-thinking.com>
2024-11-19 11:22:30 -0500

    Initial Commit

---
+++ SwiftfulStarterProject/SwiftfulStarterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulStarterProjectApp.swift
+//  SwiftfulStarterProject
+//
+//  Created by Nick Sarno on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulStarterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulStarterProjectTests/SwiftfulStarterProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftfulStarterProjectTests.swift
+//  SwiftfulStarterProjectTests
+//
+//  Created by Nick Sarno on 11/19/24.
+//
+
+import Testing
+
+struct SwiftfulStarterProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
