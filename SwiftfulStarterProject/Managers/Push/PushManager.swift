90e26b34e0a351077038f32fd4e8fd6d58b8afba
Nick <hello@swiftful-thinking.com>
2024-11-21 12:40:30 -0500

    Tweaks

--- SwiftfulStarterProject/Managers/Push/PushManager.swift
+++ SwiftfulStarterProject/Managers/Push/PushManager.swift
@@ -26,41 +26,7 @@ class PushManager {
     func canRequestAuthorization() async -> Bool {
         await LocalNotifications.canRequestAuthorization()
     }
-    
-    func schedulePushNotificationsForTheNextWeek() {
-        LocalNotifications.removeAllPendingNotifications()
-        LocalNotifications.removeAllDeliveredNotifications()
         
-        Task {
-            do {
-                // Tomorrow
-                try await scheduleNotification(
-                    title: "Hey you! Ready to chat?",
-                    subtitle: "Open AI Chat to begin.",
-                    triggerDate: Date().addingTimeInterval(days: 1)
-                )
-                
-                // In 3 days
-                try await scheduleNotification(
-                    title: "Someone sent you a message!",
-                    subtitle: "Open AI Chat to respond.",
-                    triggerDate: Date().addingTimeInterval(days: 3)
-                )
-                
-                // In 5 days
-                try await scheduleNotification(
-                    title: "Hey stranger. We miss you!",
-                    subtitle: "Don't forget about us.",
-                    triggerDate: Date().addingTimeInterval(days: 5)
-                )
-                
-                logManager?.trackEvent(event: Event.weekScheduledSuccess)
-            } catch {
-                logManager?.trackEvent(event: Event.weekScheduledFail(error: error))
-            }
-        }
-    }
-    
     private func scheduleNotification(title: String, subtitle: String, triggerDate: Date) async throws {
         let content = AnyNotificationContent(title: title, body: subtitle)
         let trigger = NotificationTriggerOption.date(date: triggerDate, repeats: false)
@@ -68,20 +34,16 @@ class PushManager {
     }
     
     enum Event: LoggableEvent {
-        case weekScheduledSuccess
-        case weekScheduledFail(error: Error)
+        case sample
 
         var eventName: String {
             switch self {
-            case .weekScheduledSuccess:          return "PushMan_WeekSchedule_Success"
-            case .weekScheduledFail:             return "PushMan_WeekSchedule_Fail"
+            case .sample:          return "PushMan_SampleEvent"
             }
         }
         
         var parameters: [String: Any]? {
             switch self {
-            case .weekScheduledFail(error: let error):
-                return error.eventParameters
             default:
                 return nil
             }
@@ -89,8 +51,6 @@ class PushManager {
         
         var type: LogType {
             switch self {
-            case .weekScheduledFail:
-                return .severe
             default:
                 return .analytic
             }
